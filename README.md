e3f05535a551ecc497b544f929660881c5590255
Paul Tinsley <paul.tinsley@gmail.com>
2023-01-03 21:10:56 -0600

    first working (deployed) version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # voider
-voided check maker
+
+A lot of modern online banks don't issue checks, especially ones like Qube, One Finance etc... that work on the idea of bank accounts for each vendor/bill. I've found that some utility providers still expect to get a voided check to establish or update auto draft. The goal of this project is to make arbitrary void checks as needed to setup auto draft for different providers.
+
+## Local Development
+
+Loading dependencies
+```bash
+pip3 install -r requirements.txt
+```
+
+Running the app with automatic reload as code is changed
+```bash
+FLASK_APP=voider flask --debug run
+```
+
+## Deploying the application
+
+The repo has the configuration for my specific deployment on fly.io, that is what I'm talking about here.
+
+```bash
+flyctl deploy
+```
---
+++ voider.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, render_template
+
+from base64 import b64encode
+from io import BytesIO
+
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+from os import path
+
+script_path = path.dirname(path.realpath(__file__))
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+
+def root():
+    name = ""
+    address1 = ""
+    address2 = ""
+    city = ""
+    state = ""
+    zipcode = ""
+    bank_name = ""
+    check_number = ""
+    account_number = ""
+    aba_number = ""
+
+    if request.method == "POST":
+        name = request.form['name']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode']
+        bank_name = request.form['bank_name']
+        check_number = request.form['check_number']
+        account_number = request.form['account_number']
+        aba_number = request.form['aba_number']
+
+    image_data = draw_check(name, address1, address2, city, state, zipcode, bank_name, check_number, account_number, aba_number)
+
+    return render_template(
+        "root.html", 
+        name=name,
+        address1=address1,
+        address2=address2,
+        city=city,
+        state=state,
+        zipcode=zipcode,
+        bank_name=bank_name,
+        check_number=check_number,
+        account_number=account_number,
+        aba_number=aba_number,
+        image_data=image_data
+    )
+
+def draw_check(name, address1, address2, city, state, zipcode, bank_name, check_number, account_number, aba_number):
+    # Open an image file
+    image = Image.open(f"{script_path}/images/blankcheck.jpg")
+
+    # Open a font file
+    text_font = ImageFont.truetype(
+        f"{script_path}/fonts/OpenSans-Regular.ttf", 14)
+    void_font = ImageFont.truetype(
+        f"{script_path}/fonts/HolliesBigInk-Regular.ttf", 180)
+    micr_font = ImageFont.truetype(
+        f"{script_path}/fonts/micrenc.ttf", 24)
+
+    # Get a drawing context
+    draw = ImageDraw.Draw(image)
+
+    draw.text((680, 100), "DATE", font=text_font, fill="black")
+    draw.line((720, 115, 840, 115), fill="black")
+
+    draw.text((780, 180), "DOLLARS", font=text_font, fill="black")
+    draw.line((30, 195, 770, 195), fill="black")
+
+    draw.text((30, 130), "PAY TO THE\nORDER OF", font=text_font, fill="black")
+    draw.line((110, 165, 650, 165), fill="black")
+
+    draw.text((680, 150), "$", font=text_font, fill="black")
+    draw.line((695, 165, 840, 165), fill="black")
+
+    draw.text((30, 290), "MEMO", font=text_font, fill="black")
+    draw.line((80, 305,400,305), fill="black")
+
+    account_owner = name
+    if address1 != "":
+        account_owner = account_owner + f"\n{address1}"
+
+    if address2 != "":
+        account_owner = account_owner + f"\n{address2}"
+
+    if city != "" and state != "" and zipcode != "":
+        account_owner = account_owner + f"\n{city}, {state} {zipcode}"
+    draw.text((30, 30), account_owner, font=text_font, fill="black")
+
+    draw.text((350, 30), bank_name, font=text_font, fill="black")
+
+    draw.text((790,30), check_number, font=text_font, fill="black")
+
+    draw.text((220, 110), "VOID", font=void_font, fill="black")
+
+    micr = f"C{check_number}C    A{aba_number}A    {account_number}C"
+    draw.text((120, 350), micr, font=micr_font, fill="black")
+
+    image_io = BytesIO()
+    image.save(image_io, 'PNG')
+
+    return 'data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii')
